7ca8cf2ebc7c9dc1819e0c1874c23ba4eb47a8ca
afzal saiyed <saiyedafzalgz@gmail.com>
2021-08-01 02:27:40 +0530

    project init

---
+++ cartdrop/accounts/urls.py
@@ -0,0 +1,13 @@
+from django.http.response import HttpResponse
+from django.urls import path
+
+
+app_name="accounts"
+
+
+def test(request):
+    return HttpResponse("Hello")
+
+urlpatterns = [
+    path('', test, name="test")
+]
