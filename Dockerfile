cc221c6c1bfb5377d406b7ac6c4aff2b3e7c9600
afzal saiyed <saiyedafzalgz@gmail.com>
2021-08-02 16:40:28 +0530

    fixed docker config

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,4 @@
-FROM python:3.9.6-slim-buster
-RUN apt-get update && apt-get install build-essential python3-dev python3-pip python3-setuptools python3-wheel python3-cffi libcairo2 libpango-1.0-0 libpangocairo-1.0-0 libgdk-pixbuf2.0-0 libffi-dev shared-mime-info -y
+FROM afzalsaiyed/corecare_base:latest
 ENV PYTHONUNBUFFERED 1
 WORKDIR /cartdrop
 COPY /requirements/base.txt .
--- config/settings/testing.py
+++ config/settings/testing.py
@@ -9,8 +9,8 @@ DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql_psycopg2",
         "NAME": get_value('CARTDROP_DEV_DB_NAME'),
-        "USER": get_value('CARTDROP_DEV_DB_USER'),
-        "PASSWORD": get_value('CARTDROP_DEV_DB_PASSWORD'),
+        "USER": get_value('POSTGRES_USER'),
+        "PASSWORD": get_value('POSTGRES_PASSWORD'),
         "HOST": get_value('CARTDROP_DEV_DB_HOST'),
         "PORT": "5432",
     }
