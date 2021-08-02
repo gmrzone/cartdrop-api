40750a8846cf501f349c48c33509d40815f83b99
afzal saiyed <saiyedafzalgz@gmail.com>
2021-08-02 15:06:34 +0530

    completed docker setup for development

--- config/settings/testing.py
+++ config/settings/testing.py
@@ -3,15 +3,15 @@ from .base import *
 
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql_psycopg2",
-        "NAME": "",
-        "USER": "",
-        "PASSWORD": "",
-        "HOST": "",
+        "NAME": get_value('CARTDROP_DEV_DB_NAME'),
+        "USER": get_value('CARTDROP_DEV_DB_USER'),
+        "PASSWORD": get_value('CARTDROP_DEV_DB_PASSWORD'),
+        "HOST": get_value('CARTDROP_DEV_DB_HOST'),
         "PORT": "5432",
     }
 }
---
+++ dockerfiles/postgres_init.dev.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -e
+
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
+    CREATE DATABASE cartdrop_dev;
+	CREATE USER afzal WITH PASSWORD 'afzal12345';
+    ALTER ROLE afzal SET client_encoding TO 'utf8';
+    ALTER ROLE afzal SET default_transaction_isolation TO 'read committed';
+    ALTER ROLE afzal SET timezone TO 'UTC';
+	GRANT ALL PRIVILEGES ON DATABASE cartdrop_dev TO afzal;
+EOSQL
