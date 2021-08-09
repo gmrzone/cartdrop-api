35a7a33231c570fb5b56200c22faeb308178a5d4
afzal saiyed <saiyedafzalgz@gmail.com>
2021-08-09 15:39:06 +0530

    product model

--- cartdrop/core/models.py
+++ cartdrop/core/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.conf import settings
 from .behaviours import Slugable, Timestamps, UUIDField
 from .utils import category_images, subcategory_images, product_images
 
@@ -29,8 +29,10 @@ class ProductSubcategory(Timestamps, Slugable, UUIDField, models.Model):
 
 
 class Product(Timestamps, Slugable, UUIDField, models.Model):
+    seller = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="products", null=True)
     name= models.CharField(max_length=100)
-    subcategory = models.ForeignKey(ProductCategory, on_delete=models.CASCADE, related_name="products")
+    subcategory = models.ForeignKey(ProductCategory, on_delete=models.CASCADE, related_name="items")
     price = models.DecimalField(max_digits=10, decimal_places=2)
     active = models.BooleanField(default=True)
     photo = models.ImageField(upload_to=product_images)
+    description = models.TextField(max_length=100, blank=True)
