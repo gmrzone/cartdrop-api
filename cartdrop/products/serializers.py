c921568d897ca2bb375431a6e14dbfef27abb1e0
AFzal Saiyed <saiyedafzalgz@gmail.com>
2021-08-27 19:58:49 +0530

    added serializers for products and product variations

---
+++ cartdrop/products/serializers.py
@@ -0,0 +1,76 @@
+from django.db import models
+from django.db.models import fields
+from rest_framework.serializers import ModelSerializer
+from .models import ProductVariation, Product, ProductSpecification, ProductWarranty
+
+
+class ProductWarrantySerializer(ModelSerializer):
+    class Meta:
+        models = ProductWarranty
+        fields = ("summary", "covered", "not_covered")
+
+
+class ProductSpecificationSerializer(ModelSerializer):
+    class Meta:
+        model = ProductSpecification
+        fields = (
+            "in_box",
+            "launched_date",
+            "model_no",
+            "model_name",
+            "mobile",
+            "laptop",
+            "tv",
+            "washing_machine",
+            "ac",
+            "refrigerator",
+            "speaker",
+        )
+
+
+class ProductVariationSerializer(ModelSerializer):
+
+    specification = ProductSpecification(many=False)
+    warranty = ProductWarranty(many=False)
+
+    class Meta:
+        model = Product
+        fields = (
+            "brand",
+            "name",
+            "slug",
+            "seller",
+            "subcategory",
+            "description",
+            "overall_rating",
+            "replacement_days",
+            "warranty",
+            "weight",
+            "specification",
+        )
+
+
+class ProductVariationSerializer(ModelSerializer):
+
+    product = ProductVariationSerializer(many=False)
+
+    class Meta:
+        model = ProductVariation
+        fields = (
+            "PID",
+            "product",
+            "retail_price",
+            "price",
+            "color",
+            "available_stock",
+            "mobile_variant",
+            "laptop_variant",
+            "tv_variant",
+            "ac_capacity_variant",
+            "refrigerator_capacity",
+            "ac_star_variant",
+            "book_variation",
+            "juices_quantity",
+            "size",
+            "images",
+        )
--- config/urls.py
+++ config/urls.py
@@ -18,8 +18,7 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 from django.urls.conf import include
-from rest_framework_simplejwt.views import (TokenObtainPairView,
-                                            TokenRefreshView)
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
