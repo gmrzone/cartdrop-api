d5de28780ace9db1148daccba175dab0e18ab683
Afzal <saiyedafzalgz@gmail.com>
2022-02-27 13:49:51 +0530

    added_pytest

---
+++ cartdrop/core/tests/test_core_urls.py
@@ -0,0 +1,24 @@
+from django.urls import resolve, reverse
+from ..views import *
+
+
+
+def test_category_list_url():
+    url = reverse("core:category_list")
+    resolver = resolve(url)
+    assert resolver.func.view_class == CategoryList
+
+def test_subcategory_list_url():
+    url = reverse("core:subcategory_list", kwargs={"category": "mobiles"})
+    resolver = resolve(url)
+    assert resolver.func.view_class == SubcategoryList
+
+def test_offers_list_url():
+    url = reverse("core:subcategory_offers")
+    resolver = resolve(url)
+    assert resolver.func.view_class == SubcategoryOfferList
+
+def test_brand_by_category_url():
+    url = reverse("core:brand_by_category_new", kwargs={"category": "mobiles"})
+    resolver = resolve(url)
+    assert resolver.func.view_class == ProductBrandsByCategory
